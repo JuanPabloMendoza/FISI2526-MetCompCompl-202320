f5d42bc86f07f1e431331023d0fdad2a899cf029
Juan Pablo Mendoza Arias <jp.mendozaa1@uniandes.edu.co>
2023-10-31 19:02:05 -0500

    Ejercicio 3 taller 3

--- Taller 3/Ejercicio 3.py
+++ Taller 3/Ejercicio 3.py
@@ -66,7 +66,9 @@ print(f'Tomemos\nm1 = {m1}, m2 = {m2}, m3 = {m3},\nk11 = {k11},k12 = {k12}, k13 
 A = np.array([[k11/m1,k12/m2,k13/m3],[k21/m1,k22/m2,k23/m3],[k31/m1,k32/m2,k33/m3]], dtype='float64')
 max_fr, max_amp = MaxEigenValue(A,z)
 min_fr, min_amp = MinEigenValue(A,z)
+max_fr = np.sqrt((-1)*max_fr)
+min_fr = np.sqrt((-1)*min_fr)
 
-print(f'{max_fr}\n{min_fr}\n{max_amp}\n{min_amp}')
+print(f'Máxima frecuencia: {max_fr} Hz\nMínima frecuencia: {min_fr} Hz\nMáxima amplitud:\n{max_amp} \nMínima amplitud:\n{min_amp} ')
 
         
